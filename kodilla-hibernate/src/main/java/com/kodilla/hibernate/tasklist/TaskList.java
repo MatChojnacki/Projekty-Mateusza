db3fb9fcf3b4cb885029a51939af946c4c85df75
Chojnacki <twojemail@example.com>
2024-03-12 03:22:51 +0100

    .

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,20 +1,21 @@
 package com.kodilla.hibernate.tasklist;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.kodilla.hibernate.task.Task;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name = "TASKLISTS")
-
+@Table(name="TASKLISTS")
 public class TaskList {
 
     private int id;
     private String listName;
     private String description;
+    private List<Task> tasks = new ArrayList<>();
 
     public TaskList() {
     }
@@ -23,30 +24,49 @@ public class TaskList {
         this.listName = listName;
         this.description = description;
     }
+
     @Id
-    @GeneratedValue
     @NotNull
+    @GeneratedValue
+    @Column(name="ID", unique=true)
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    @NotNull
+    @Column(name="LISTNAME")
     public String getListName() {
         return listName;
     }
 
-    public void setListName(String listName) {
-        this.listName = listName;
-    }
-
+    @Column(name = "DESCRIPTION")
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    private void setDescription(String description) {
         this.description = description;
+    }
+
+    private void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }
